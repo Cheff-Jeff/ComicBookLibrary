16003fe3d4345078a9b7a24b84b7f3c33ed08044
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-09-08 11:12:51 +0200

    add banner component

--- src/components/BannerRight.vue
+++ src/components/BannerRight.vue
@@ -8,9 +8,16 @@
 </script>
 
 <template>
-  
+  <section class="banner">
+    <div class="text-wrap">
+      <h1>{{ Title }}</h1>
+    </div>
+    <div class="img-wrap">
+      <img src="@/assets/images/secBanner.png" alt="banner">
+    </div>
+  </section>
 </template>
 
-<style scoped >
-
+<style scoped lang="scss">
+  @import "@/assets/styles/components/Banner.scss";
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <script setup>
 //import TheWelcome from '../components/TheWelcome.vue'
+import BannerRight from '../components/BannerRight.vue';
 </script>
 
 <template>
+  <BannerRight Title="Test"/>
   <!-- <main>
     <TheWelcome />
   </main> -->
