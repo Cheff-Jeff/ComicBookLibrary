3c12ad2340832d8be99063ea3cbfa3d79e178631
CheffJeff <1999jeffreynijkamp@gmail.com>
2022-10-17 15:54:03 +0200

    update web api

---
+++ ComicsBackend/ComicsBackend/Controllers/ComicsController.cs
@@ -0,0 +1,91 @@
+﻿using ComicsBackend.Data;
+using ComicsBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicsBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComicsController : ControllerBase
+    {
+        private readonly ComicDbContext _context;
+        public ComicsController(ComicDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllComics([FromQuery] QueryParameters parameters)
+        {
+            IQueryable<Comic> comics = _context.Comics;
+            comics = comics
+                .Skip(parameters.Size * (parameters.Page - 1))
+                .Take(parameters.Size);
+
+            return Ok(await comics.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Comic), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            Comic? comic = await _context.Comics
+                .Include(c => c.Writer)
+                .Include(c => c.Publisher)
+                .Include(c => c.Artist)
+                .Include(c => c.CoverArtist)
+                .FirstOrDefaultAsync(e => e.Id == id);
+
+            return comic == null ? NotFound() : Ok(comic);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Comic comic)
+        {
+            _context.Entry(comic).State = EntityState.Modified;
+
+            await _context.Comics.AddAsync(comic);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = comic.Id }, comic);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(int id, Comic comic)
+        {
+            if (id != comic.Id)
+            {
+                return BadRequest();
+            }
+            else 
+            {
+                _context.Entry(comic).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        { 
+            Comic? comic = await _context.Comics.FindAsync(id);
+            if (comic == null)
+            {
+                return NotFound();
+            }
+            else
+            { 
+                _context.Comics.Remove(comic);
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ ComicsBackend/ComicsBackend/Controllers/WritersController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ComicsBackend.Data;
+using ComicsBackend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicsBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WritersController : ControllerBase
+    {
+        private readonly ComicDbContext _context;
+        public WritersController(ComicDbContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllWriters([FromQuery] QueryParameters parameters)
+        {
+            IQueryable<Writer> writers = _context.Writers;
+            writers = writers
+                .Skip(parameters.Size * (parameters.Page - 1))
+                .Take(parameters.Size);
+            return Ok(await writers.ToArrayAsync());
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Writer), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetWriterById(int id)
+        {
+            Writer? writer = await _context.Writers.FindAsync(id);
+            return writer == null ? NotFound() : Ok(writer);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> CreateWriter(Writer writer)
+        { 
+            await _context.Writers.AddAsync(writer);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetWriterById), new { id = writer.Id }, writer);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdateWriter(int id, Writer writer)
+        {
+            if (id != writer.Id) return BadRequest();
+
+            _context.Entry(writer).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteWriter(int id)
+        {
+            Writer? writer = await _context.Writers.FindAsync(id);
+            if(writer == null) return BadRequest();
+
+            _context.Writers.Remove(writer);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
