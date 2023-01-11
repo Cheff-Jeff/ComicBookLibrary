e2ea08f8e398303d70e2df7b7d99166a75d963c1
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-01-11 22:45:40 +0100

    cloud

--- user-front-end/cypress.config.js
+++ user-front-end/cypress.config.js
@@ -4,6 +4,6 @@ module.exports = defineConfig({
   projectId: "x6y8x3",
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
+    baseUrl: 'http://localhost:8082'
   }
 })
