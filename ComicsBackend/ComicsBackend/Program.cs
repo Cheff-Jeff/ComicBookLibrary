3d8d4d03356114a0f0fdaea56be2d1118c15d5a1
CheffJeff <1999jeffreynijkamp@gmail.com>
2023-01-12 00:15:33 +0100

    slaapies

--- ComicsBackend/ComicsBackend/Program.cs
+++ ComicsBackend/ComicsBackend/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
+string dbEnv = Environment.GetEnvironmentVariable("DATABASE_CONECTION");
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -16,7 +17,17 @@ builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
     //build.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed((host) => true);
     build.AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed((host) => true).AllowCredentials();
 }));
-builder.Services.AddDbContext<ComicDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer")));
+builder.Services.AddDbContext<ComicDbContext>(options =>
+{
+    if (dbEnv != null)
+    {
+        options.UseSqlServer(dbEnv);
+    }
+    else
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+    }
+});
 builder.Services.AddSignalR(options => 
 {
     options.EnableDetailedErrors = true;
